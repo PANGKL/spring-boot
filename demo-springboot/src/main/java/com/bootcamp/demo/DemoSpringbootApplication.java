6a6499e0d80e81b3fcb503923c98e06e25a0f193
“PANGKL” <pklpklpk123@gmail.com>
2023-09-08 16:44:22 +0800

    springBoot

---
+++ demo-springboot/src/main/java/com/bootcamp/demo/DemoSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.bootcamp.demo.demospringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringbootApplication.class, args);
+
+	}
+
+}
