4df78d618c42e8c3218cecaa51c25a89b0ca451a
“PANGKL” <pklpklpk123@gmail.com>
2023-09-20 09:44:56 +0800

    week 12

--- demo-stockexercise/src/test/java/com/hkjava/demostockexercise/StockControllerTest.java
+++ demo-stockexercise/src/test/java/com/hkjava/demostockexercise/StockControllerTest.java
@@ -1,7 +1,9 @@
 package com.hkjava.demostockexercise;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -10,10 +12,13 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultHandler;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import com.hkjava.demostockexercise.exception.BussinessException;
 import com.hkjava.demostockexercise.model.CompanyDTO;
 import com.hkjava.demostockexercise.model.Stock;
 import com.hkjava.demostockexercise.service.StockService;
+import com.jayway.jsonpath.JsonPath;
 import ch.qos.logback.core.encoder.EchoEncoder;
 
 @WebMvcTest
@@ -29,18 +34,23 @@ public class StockControllerTest {
   @Test
   void testGetStock() throws Exception {
 
-    CompanyDTO companyDTO =
-        new CompanyDTO("Hong Kong", "PKL", "sdsd", "sdsd", 12, "sdsd");
-    Stock stock = new Stock(companyDTO, 100, 110, 20, 30, 40);
+    CompanyDTO companyDTO = new CompanyDTO("US", "Apple Inc", "1980-12-12",
+        "https://static2.finnhub.io/file/publicdatany/finnhubimage/stock_logo/AAPL.svg",
+        2782424.363206, "USD");
+    Stock stock = new Stock(companyDTO, 177.97, 177.97, 176.17, 176.48, 175.01);
 
     String str = "aapl";
     Mockito.when(stockService.findStockState(str)).thenReturn(stock);
-    // Mockito.when(stockService.findCompanyQuote(str));
+    // Stock s = stockService.findStockState(str);
+    // System.out.println(s);
 
     mockMvc.perform(get("/api/v1/stock").param("symbol", str))
         .andExpect(status().isOk())//
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))// ;
-    ;
+        .andExpect(content().contentType(MediaType.APPLICATION_JSON))//
+        .andExpect(jsonPath("$.companyProfile.country").value("US"))
+        .andExpect(jsonPath("$.companyProfile.currency").value("USD"))
+        .andExpect(jsonPath("$.currentPrice").value(177.97))
+        .andDo(MockMvcResultHandlers.print());
 
 
 
